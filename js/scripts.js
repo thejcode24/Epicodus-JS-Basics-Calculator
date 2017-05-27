60ee9f653ebfcc3ac1a7a4a2ca1befb7a2e39fcc
James Jung <thejcode24@gmail.com>
2017-05-26 19:47:12 -0700

    added operators and styling of the buttons on calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,37 @@
-var addition = function(num1, num2) {
-  return num1+num2;
-};
+// var addition = function(num1, num2) {
+//   return num1+num2;
+// };
 
-var subtract = function(num1, num2) {
-  return num1-num2;
-};
+// var subtract = function(num1, num2) {
+//   return num1-num2;
+// };
 
-var multiply = function(num1,num2) {
-  return num1*num2
-};
+// var multiply = function(num1,num2) {
+//   return num1*num2
+// };
 
-var divide = function(num1,num2) {
-  return num1/num2
-};
+// var divide = function(num1,num2) {
+//   return num1/num2
+// };
 
-var exponent = function(num1, num2) {
-  return Math.pow(num1,num2);
-}
+// var exponent = function(num1, num2) {
+//   return Math.pow(num1,num2);
+// }
 
-var num1 = parseInt(prompt("Enter first number: "));
-var num2 = parseInt(prompt("Enter second number: "));
+// var num1 = parseInt(prompt("Enter first number: "));
+// var num2 = parseInt(prompt("Enter second number: "));
 
-// alert(add(num1,num2));
-// alert(subtract(num1,num2));
-// alert(multiply(num1,num2));
-// alert(divide(num1,num2));
-// alert(exponent(num1,num2));
+// // alert(add(num1,num2));
+// // alert(subtract(num1,num2));
+// // alert(multiply(num1,num2));
+// // alert(divide(num1,num2));
+// // alert(exponent(num1,num2));
 
-$(document).ready(function() {
-  $("button#addition").click(function() {
-    $(".displayResult").append(addition(num1,num2));
-  });
-});
-
+// $(document).ready(function() {
+//   $("button#addition").click(function() {
+//     $(".displayResult").append(addition(num1,num2));
+//   });
+// });
 
 
 
